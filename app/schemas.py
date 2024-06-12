1967b65f58f2ba5597ec05f9a16fc868b5139f65
daviddprtma <davidchristianpratama@gmail.com>
2024-06-12 11:27:38 +0700

    first commit

---
+++ app/schemas.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+class ItemBase(BaseModel):
+    title: str
+    description: str | None = None
+    price: float
+class ItemCreate(ItemBase):
+    pass
+class Item(ItemBase):
+    id: int
